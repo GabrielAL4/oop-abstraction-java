064acacdff6c59f93aededb62abcea7aa5564a0c
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2023-01-05 19:35:25 -0300

    Encapsulation commit and course and mentoring classes created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.dio.challenge.domain.Course;
+import br.com.dio.challenge.domain.Mentoring;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Course course1 = new Course();
+        course1.setTitle("JSF para aplicação web");
+        course1.setDescription("Introdução a JSF para aplicação web");
+        course1.setWorkload(2);
+        System.out.println(course1);
+
+        Mentoring mentoring1 = new Mentoring();
+        mentoring1.setTitle("Mentoria Java 11");
+        mentoring1.setDescription("Mentoria sobre atributos básico do Java 11");
+        mentoring1.setDate(LocalDate.of(2023, 2, 13));
+
+        System.out.println(mentoring1);
     }
 }
